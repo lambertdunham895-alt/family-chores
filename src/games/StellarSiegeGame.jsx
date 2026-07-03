fda0e6964f2373d58afb84ad1313db6a7ce72437
lambertdunham895-alt <lambertdunham895@gmail.com>
2026-07-03 03:14:57 -0500

    Update StellarSiegeGame.jsx

--- src/games/StellarSiegeGame.jsx
+++ src/games/StellarSiegeGame.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 export default function StellarSiegeGame() {
   return (
-    <div style={{ width: '100%', height: '100%', background: '#000' }}>
+    <div style={{ width: "100%", height: "calc(100vh - 52px)", background: "#000" }}>
       <iframe
         src="/stellar-siege/index.html"
         title="Stellar Siege"
-        style={{ width: '100%', height: '100%', border: 'none', display: 'block' }}
+        style={{ width: "100%", height: "100%", border: "none", display: "block" }}
         allow="autoplay"
       />
     </div>
